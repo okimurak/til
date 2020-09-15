274914c25586a20e3675bfd1e5b3a9e9f36c2091
okimurak <okimurak0901@gmail.com>
2020-09-15 17:08:12 +0900

    feat: add http 1.1 functions

--- real_world_http/server/test_echo_server.go
+++ real_world_http/server/test_echo_server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/k0kubun/pp"
 )
@@ -42,10 +44,66 @@ func handlerDigest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	hijacker := w.(http.Hijacker)
+
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	defer conn.Close()
+
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush()
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func handlerChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expexted http.ResponseWrite to be an http.Flusher")
+	}
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	flusher.Flush()
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/digest", handlerDigest)
+	http.HandleFunc("/chunked", handlerChunkedResponse)
+
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
