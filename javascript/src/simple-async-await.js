1d1dc40773cecf442e37980bc049ee827ed24281
okimurak <okimurak0901@gmail.com>
2021-10-21 18:19:19 +0900

    feat: add async await

---
+++ javascript/src/simple-async-await.js
@@ -0,0 +1,16 @@
+
+function timer2Seconds() {
+  return new Promise(resolve =>{
+    setTimeout(()=> {
+      resolve('resolved');
+    },2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await timer2Seconds();
+  console.log(result);
+}
+
+asyncCall();
